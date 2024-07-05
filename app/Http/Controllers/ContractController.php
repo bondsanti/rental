560a80a59144707f9cb3626732ae0a1545e5be91
santi.c <santi.chpy@gmail.com>
2024-07-05 17:43:35 +0700

    ddd

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -112,10 +112,10 @@ class ContractController extends Controller
 
     public function room_con()
     {
-        $dataLoginUser = User::where('user_id', Session::get('loginId'))->first();
-        $isRole = Role_user::where('user_id', Session::get('loginId'))->first();
+        // $dataLoginUser = User::where('user_id', Session::get('loginId'))->first();
+        // $isRole = Role_user::where('user_id', Session::get('loginId'))->first();
         ///ssssss
-        dd($isRole);
+        dd("ok");
         $projects = Project::where('rent', 1)
             ->orderBy('Project_Name', 'asc')
             ->get();
